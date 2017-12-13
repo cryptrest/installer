5805e46988b627820fb689b1c2c5c2213b557eaa
Che Pasha <che,pasha@gmail.com>
2017-12-13 03:27:13 +0300

    Auto generation NGinx config has been added

---
+++ letsencrypt/certs-define.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+LETSENCRYPT_URL='https://letsencrypt.org/certs/'
+LETSENCRYPT_PEMS='lets-encrypt-x4-cross-signed.pem lets-encrypt-x3-cross-signed.pem isrgrootx1.pem'
+LETSENCRYPT_DIR='/etc/letsencrypt/live'
+LETSENCRYPT_BITS='256'
+
+PUBLIC_KEY_PINS=''
+
+ECDSA_KEY="${LETSENCRYPT_DIR}/${DOMAIN}/ecdsa.key"
+ECDSA_CSR="${LETSENCRYPT_DIR}/${DOMAIN}/ecdsa.csr"
+
+# HD Param
+hd_param_define()
+{
+    openssl dhparam -out ${LETSENCRYPT_DIR}/${DOMAIN}/dhparam.pem 2048
+}
+
+# ECDSA
+ecdsa_define()
+{
+    openssl req -new -sha512 -key ${LETSENCRYPT_DIR}/${DOMAIN}/privkey.pem -out $ECDSA_CSR -subj "/CN=$DOMAIN" -config $CURRENT_DIR/csr-$DOMAIN.conf
+#    openssl ecparam -genkey -name secp384r1 | openssl ec -out $ECDSA_KEY && openssl req -new -sha256 -key $ECDSA_KEY -nodes -out $ECDSA_CSR -outform pem
+}
+
+# PUBLIC_KEY_PINS
+public_key_pins_define()
+{
+    local hash=''
+
+    for bit in $LETSENCRYPT_BITS; do
+        # Let's Encrypt
+        for pem in $LETSENCRYPT_PEMS; do
+            hash="$(curl -s "${LETSENCRYPT_URL}${pem}" | openssl x509 -pubkey | openssl pkey -pubin -outform der | openssl dgst -sha${bit} -binary | base64)"
+            PUBLIC_KEY_PINS="${PUBLIC_KEY_PINS}pin-sha${bit}=\"${hash}\"; "
+        done
+
+        # RSA
+        hash="$(openssl rsa -pubout -in ${LETSENCRYPT_DIR}/${DOMAIN}/privkey.pem -outform DER | openssl dgst -sha${bit} -binary | openssl enc -base64)"
+        PUBLIC_KEY_PINS="${PUBLIC_KEY_PINS}pin-sha${bit}=\"${hash}\"; "
+
+        # ECDSA
+#        hash="$(openssl ec -pubout -in $ECDSA_CSR -outform DER | openssl dgst -sha${bit} -binary | openssl enc -base64)"
+#        PUBLIC_KEY_PINS="${PUBLIC_KEY_PINS}pin-sha${bit}=\"${hash}\"; "
+    done
+}
--- letsencrypt/update.sh
+++ letsencrypt/update.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-/home/cryptrest/letsencrypt/certbot/certbot-auto certonly --standalone --email crypt.rest@gmail.com --renew-by-default --rsa-key-size 4096 -d crypt.rest -d www.crypt.rest --pre-hook "service nginx stop" --post-hook "service nginx start"
+CURRENT_DIR="$(cd $(dirname $0) && pwd -P)"
+
+CERTS_DEFINE_FILE="$CURRENT_DIR/certs-define.sh"
+NGINX_DIR="$CURRENT_DIR/../nginx"
+NGINX_CONFIG_DEFINE_FILE="$NGINX_DIR/config-define.sh"
+
+DOMAIN='crypt.rest'
+UPSTREAM='cryptrest'
+ROOT_WWW="/home/$UPSTREAM/www/rest"
+
+
+. $CERTS_DEFINE_FILE
+. $NGINX_CONFIG_DEFINE_FILE
+
+
+ecdsa_define
+public_key_pins_define
+hd_param_define
+
+nginx_config_define
+
+#/home/$UPSTREAM/letsencrypt/certbot/certbot-auto certonly --webroot -d $DOMAIN -d www.$DOMAIN --email $DOMAIN@gmail.com --csr $ECDSA_CSR --agree-tos
+/home/$UPSTREAM/letsencrypt/certbot/certbot-auto certonly --standalone --email $DOMAIN@gmail.com --renew-by-default --rsa-key-size 2048 -d $DOMAIN -d www.$DOMAIN --pre-hook "service nginx stop" --post-hook "service nginx start"
