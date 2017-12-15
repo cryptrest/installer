02b6153dcfd3c83bf396360c4da8d6e217a8de37
che <che.pasha@gmail.com>
2017-12-15 04:52:47 +0300

    Installer has been created

--- exec.sh
+++ exec.sh
@@ -2,10 +2,11 @@
 
 CURRENT_DIR="$(cd $(dirname $0) && pwd -P)"
 
-CRYPTREST_DIR="${HOME}/.cryptrest"
+CRYPTREST_DIR="$HOME/.cryptrest"
 CRYPTREST_INSTALLER_DIR="$CRYPTREST_DIR/installer"
+CRYPTREST_INSTALLER_PATH="$HOME/installer"
 CRYPTREST_BRANCH='master'
-CRYPTREST_GIT_URL="https://github.com/cryptrest/installer/archive/${CRYPTREST_BRANCH}.tar.gz"
+CRYPTREST_GIT_URL="https://github.com/cryptrest/installer/archive/$CRYPTREST_BRANCH.tar.gz"
 CRYPTREST_MODULES='go letsencrypt nginx'
 CRYPTREST_IS_LOCAL=0
 
@@ -48,7 +49,7 @@ cryptrest_network()
     mv "$CRYPTREST_INSTALLER_DIR-$CRYPTREST_BRANCH" "$CRYPTREST_INSTALLER_DIR" && \
     chmod 700 "$CRYPTREST_DIR"
     if [ $? -eq 0 ]; then
-        "$CRYPTREST_INSTALLER_DIR/install.sh"
+        "$CRYPTREST_INSTALLER_DIR/exec.sh"
     fi
 }
 
@@ -56,9 +57,13 @@ cryptrest_install()
 {
     cryptrest_is_local
     if [ $? -eq 0 ]; then
-        cryptrest_local
+        cryptrest_local && \
+        mkdir -p "$CRYPTREST_INSTALLER_PATH" && \
+        ln -s "$CURRENT_DIR/exec.sh" "$CRYPTREST_INSTALLER_PATH/index.html"
     else
-        cryptrest_network
+        cryptrest_network && \
+        mkdir -p "$CRYPTREST_INSTALLER_PATH" && \
+        ln -s "$CRYPTREST_INSTALLER_DIR/exec.sh" "$CRYPTREST_INSTALLER_PATH/index.html"
     fi
 }
 
