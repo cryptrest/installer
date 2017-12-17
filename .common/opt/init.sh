7dcc64b7bf87b00cd74dee86b3439c92e453d21b
che <che.pasha@gmail.com>
2017-12-17 07:07:29 +0300

    Let's Encrypt installation has been optimized

--- .common/opt/init.sh
+++ .common/opt/init.sh
@@ -12,19 +12,21 @@ CRYPTREST_ETC_LETSENCRYPT_DIR="$CRYPTREST_ETC_DIR/letsencrypt"
 CRYPTREST_OPT_DIR="$CRYPTREST_DIR/opt"
 CRYPTREST_OPT_NGINX_DIR="$CRYPTREST_OPT_DIR/nginx"
 CRYPTREST_OPT_LETSENCRYPT_DIR="$CRYPTREST_OPT_DIR/letsencrypt"
-
-CRYPTREST_DOMAIN="${CRYPTREST_DOMAIN:=crypt.rest}"
-CRYPTREST_DOMAINS="$CRYPTREST_DOMAIN api.$CRYPTREST_DOMAIN installer.$CRYPTREST_DOMAIN"
+CRYPTREST_LETSENCRYPT_DOMAINS=''
+for domain in $CRYPTREST_DOMAINS; do
+    CRYPTREST_LETSENCRYPT_DOMAINS="$CRYPTREST_LETSENCRYPT_DOMAINS -d $domain"
+done
+CRYPTREST_LETSENCRYPT_DOMAINS="$CRYPTREST_LETSENCRYPT_DOMAINS -d www.$CRYPTREST_DOMAIN"
 
 
 . "$CRYPTREST_OPT_LETSENCRYPT_DIR/certs-define.sh"
 . "$CRYPTREST_OPT_NGINX_DIR/config-define.sh"
 
 
-#letsencrypt_hd_param_define && \
-#letsencrypt_ecdsa_define && \
-#letsencrypt_public_key_pins_define && \
-#nginx -v && \
+letsencrypt_hd_param_define && \
+letsencrypt_ecdsa_define && \
+letsencrypt_public_key_pins_define && \
+nginx -v && \
 nginx_configs_define && \
-echo "letsencrypt certonly --standalone --email $DOMAIN@gmail.com --renew-by-default --rsa-key-size 4096 -d $DOMAIN -d www.$DOMAIN -d api.$DOMAIN -d installer.$DOMAIN --pre-hook \"$CRYPTREST_NGINX_CMD_STOP\" --post-hook \"$CRYPTREST_NGINX_CMD_START\""
+letsencrypt certonly --standalone --email "$CRYPTREST_EMAIL" --renew-by-default --rsa-key-size "$CRYPTREST_SSL_KEY_SIZE"$CRYPTREST_LETSENCRYPT_DOMAINS --pre-hook "$CRYPTREST_NGINX_CMD_STOP" --post-hook "$CRYPTREST_NGINX_CMD_START"
 #letsencrypt certonly --webroot -d $DOMAIN -d www.$DOMAIN -d i.$DOMAIN -d installer.$DOMAIN --email $DOMAIN@gmail.com --csr $ECDSA_CSR --agree-tos
