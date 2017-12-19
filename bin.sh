1dc75c18d2433f11df9d22c5e30ab17a4f0f9ec8
che <che.pasha@gmail.com>
2017-12-19 21:28:39 +0300

    Directory bin has been fixed

--- bin.sh
+++ bin.sh
@@ -188,6 +188,7 @@ cryptrest_bin_installer_define()
         html_file="$html_dir/index.html"
         bin_file="$CRYPTREST_LIB_INSTALLER_BIN_DIR/$(basename "$f")"
 
+        [ "$f" != "$bin_file" ] && \
         cp "$f" "$bin_file" && \
         chmod 500 "$bin_file" && \
         mkdir -p "$html_dir" && \
