4660663ebfceabe4c2f984d657b1253309273710
che <che.pasha@gmail.com>
2017-12-20 18:15:45 +0300

    New OpenSSL keys has been added

--- modules/openssl/opt/certs-define.sh
+++ modules/openssl/opt/certs-define.sh
@@ -29,9 +29,22 @@ openssl_hd_param_define()
 # ECDSA
 openssl_ecdsa_define()
 {
+    openssl ecparam -genkey -name secp384r1 | openssl ec -out "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE"
+}
+
+# Certificate Signing Request (CSR)
+openssl_csr_define()
+{
+    openssl req -new -sha512 -key "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE" -nodes -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform pem
+}
+
+# ECDSA
+openssl_ecdsa_define__()
+{
     if [ -f "$CRYPTREST_OPENSSL_CSR_CONF_FILE" ]; then
         openssl req -new -sha512 -key "$CRYPTREST_OPENSSL_PRIVATE_KEY_FILE" -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -subj "/CN=$CRYPTREST_DOMAIN" -config "$CRYPTREST_OPENSSL_CSR_CONF_FILE"
-#        openssl ecparam -genkey -name secp384r1 | openssl ec -out "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE" && openssl req -new -sha256 -key "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -nodes -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform pem
+#        openssl ecparam -genkey -name secp384r1 | openssl ec -out "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE"
+#        openssl req -new -sha256 -key "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -nodes -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform pem
     fi
 }
 
