42642bcbd2fc0e1eeeadeb0cc84978c4e4e8deb1
che <che.pasha@gmail.com>
2017-12-22 21:11:48 +0300

    OpenSSL setting has been optimized

--- modules/openssl/opt/certs-define.sh
+++ modules/openssl/opt/certs-define.sh
@@ -57,14 +57,14 @@ openssl_ecdsa_define()
 # Certificate Signing Request (CSR)
 openssl_csr_define()
 {
-    openssl req -new -sha512 -key "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE" -nodes -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform pem
+    openssl req -new -sha$CRYPTREST_SSL_BIT_SIZE -key "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE" -nodes -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform pem
 }
 
 # ECDSA
 openssl_ecdsa_define__()
 {
     if [ -f "$CRYPTREST_OPENSSL_CSR_CONF_FILE" ]; then
-        openssl req -new -sha512 -key "$CRYPTREST_OPENSSL_PRIVATE_KEY_FILE" -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -subj "/CN=$CRYPTREST_DOMAIN" -config "$CRYPTREST_OPENSSL_CSR_CONF_FILE"
+        openssl req -new -sha$CRYPTREST_SSL_BIT_SIZE -key "$CRYPTREST_OPENSSL_PRIVATE_KEY_FILE" -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -subj "/CN=$CRYPTREST_DOMAIN" -config "$CRYPTREST_OPENSSL_CSR_CONF_FILE"
 #        openssl ecparam -genkey -name secp384r1 | openssl ec -out "$CRYPTREST_OPENSSL_ECDSA_KEY_FILE"
 #        openssl req -new -sha256 -key "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -nodes -out "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform pem
     fi
@@ -75,9 +75,7 @@ openssl_public_key_pins_define()
 {
     local hash=''
 
-#    for bit in $CRYPTREST_SSL_BITS; do
-#        # ECDSA
-#        hash="$(openssl ec -pubout -in "$CRYPTREST_LETSENCRYPT_ECDSA_CSR_FILE" -outform DER | openssl dgst -sha${bit} -binary | openssl enc -base64)"
-#        CRYPTREST_PUBLIC_KEY_PINS="${CRYPTREST_PUBLIC_KEY_PINS}pin-sha${bit}=\"${hash}\"; "
-#    done
+    # ECDSA
+    hash="$(openssl ec -pubout -in "$CRYPTREST_OPENSSL_ECDSA_CSR_FILE" -outform DER | openssl dgst -sha$CRYPTREST_SSL_BIT_SIZE -binary | openssl enc -base64)"
+    CRYPTREST_PUBLIC_KEY_PINS="${CRYPTREST_PUBLIC_KEY_PINS}pin-sha$CRYPTREST_SSL_BIT_SIZE=\"${hash}\"; "
 }
