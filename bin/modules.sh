a1f3e192f6c7ed8d4318afa563087715d7691299
che <che.pasha@gmail.com>
2017-12-22 14:46:48 +0300

    Network Modules installer has been created

--- bin/modules.sh
+++ bin/modules.sh
@@ -32,7 +32,7 @@ CRYPTREST_INSTALLER_GIT_BRANCH="${CRYPTREST_INSTALLER_GIT_BRANCH:=master}"
 
 CRYPTREST_INSTALLER_GIT_URL="https://github.com/cryptrest/installer/archive/$CRYPTREST_INSTALLER_GIT_BRANCH.tar.gz"
 CRYPTREST_NAME='cryptrest'
-CRYPTREST_TITLE='CryptREST'
+CRYPTREST_TITLE='CryptREST Modules'
 CRYPTREST_USER="$USER"
 CRYPTREST_DIR="$HOME/.$CRYPTREST_NAME"
 CRYPTREST_ENV_FILE="$CRYPTREST_DIR/.env"
@@ -47,7 +47,8 @@ CRYPTREST_VAR_DIR="$CRYPTREST_DIR/var"
 CRYPTREST_SSL_DIR="$CRYPTREST_DIR/ssl"
 CRYPTREST_SSL_CRYPTREST_DIR="$CRYPTREST_SSL_DIR/$CRYPTREST_NAME"
 CRYPTREST_TMP_DIR="${TMPDIR:=/tmp}/$CRYPTREST_NAME"
-CRYPTREST_MUDULES_LIB_BIN_DIR="$CRYPTREST_LIB_DIR/installer-$CRYPTREST_GIT_BRANCH/bin"
+CRYPTREST_MUDULES_LIB_BIN_DIR="$CRYPTREST_LIB_DIR/installer-$CRYPTREST_INSTALLER_GIT_BRANCH/bin"
+CRYPTREST_INSTALLER_LIB_VERSION_FILE="$CRYPTREST_MUDULES_LIB_BIN_DIR/../VERSION"
 CRYPTREST_MUDULES_LIB_BIN_FILE="$CRYPTREST_MUDULES_LIB_BIN_DIR/modules.sh"
 
 CRYPTREST_MODULES_ARGS="$*"
@@ -109,20 +110,22 @@ cryptrest_modules()
 
 cryptrest_is_local()
 {
-    for i in $CRYPTREST_MODULES; do
-        if [ -d "$CRYPTREST_MODULES_DIR/$i" ] && [ -f "$CRYPTREST_MODULES_DIR/$i/install.sh" ]; then
-            CRYPTREST_IS_LOCAL=0
+    if [ -d "$CRYPTREST_MODULES_DIR/" ]; then
+        for m in $(ls "$CRYPTREST_MODULES_DIR"); do
+            if [ -d "$CRYPTREST_MODULES_DIR/$m" ] && [ -f "$CRYPTREST_MODULES_DIR/$m/install.sh" ]; then
+                CRYPTREST_IS_LOCAL=0
 
-            break
-        fi
-    done
+                break
+            fi
+        done
+    fi
 
     return $CRYPTREST_IS_LOCAL
 }
 
 cryptrest_local_install()
 {
-    echo "$CRYPTREST_TITLE Modules mode: local"
+    echo "$CRYPTREST_TITLE mode: local"
     echo ''
 
     for m in $CRYPTREST_MODULES; do
@@ -154,21 +157,24 @@ cryptrest_network_install()
     CRYPTREST_MODULES="$CRYPTREST_MODULES" "$CRYPTREST_MODULES_LIB_BIN_FILE" $CRYPTREST_MODULES_ARGS
 }
 
+cryptrest_version_define()
+{
+    local version=''
+    local message=''
+
+    if [ -f "$CRYPTREST_INSTALLER_LIB_VERSION_FILE" ]; then
+        version="$(cat "$CRYPTREST_INSTALLER_LIB_VERSION_FILE")"
+        message=" (version: $version)"
+    fi
+
+    echo "$message"
+}
+
 cryptrest_define()
 {
-    local profile_file=''
-
-#    chmod 444 "$CRYPTREST_INSTALLER_WWW_HTML_FILE" && \
-#    ln -s "$CRYPTREST__COMMON_WWW_ERRORS_DIR/" "$CRYPTREST_INSTALLER_WWW_DIR/" && \
-#    chmod 555 "$CRYPTREST_INSTALLER_WWW_DIR" && \
-#    chmod 555 "$CRYPTREST_WWW_DIR" && \
-#    chmod 400 "$CRYPTREST_ENV_FILE" && \
-#    chmod 500 "$CRYPTREST_INSTALLER_LIB_FILE" && \
-#    ln -s "$CRYPTREST_INSTALLER_LIB_FILE" "$CRYPTREST_BIN_DIR/cryptrest-installer" && \
-
     if [ $? -eq 0 ]; then
         echo ''
-        echo "$CRYPTREST_TITLE (version: $(cat "$CRYPTREST_INSTALLER_LIB_VERSION_FILE")): installation successfully completed!"
+        echo "$CRYPTREST_TITLE$(cryptrest_version_define): installation successfully completed!"
         echo ''
     fi
 }
@@ -184,8 +190,9 @@ cryptrest_install()
             status=0
 
             if [ "$CURRENT_DIR" != "$CRYPTREST_INSTALLER_LIB_DIR" ]; then
-#                rm -f "$CRYPTREST_INSTALLER_LIB_DIR/bin.sh" && \
-#                cp "$CRYPTREST_MAIN_MODULES_DIR/bin.sh" "$CRYPTREST_INSTALLER_LIB_DIR/bin.sh" && \
+                rm -f "$CRYPTREST_MUDULES_LIB_BIN_FILE" && \
+                cp "$0" "$CRYPTREST_MUDULES_LIB_BIN_FILE" && \
+                chmod 500 "$CRYPTREST_MUDULES_LIB_BIN_FILE" && \
                 status=$?
             fi
         else
